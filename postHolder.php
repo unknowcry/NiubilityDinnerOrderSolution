b485ef1b7e5c9f55f0b309a3d29d8d4201cbd045
WolfHugo <1031629044@qq.com>
2019-08-30 16:40:22 +0800

    watch README.md

---
+++ postHolder.php
@@ -0,0 +1,27 @@
+<?php
+
+if(isset($_POST["option"])){
+    switch($_POST["option"]){
+        case "login":{
+            switch($_POST["type"]){
+                case "0":{
+                    break;
+                }
+                case "1":{
+                    break;
+                }
+                case "2":{
+                    break;
+                }
+            }
+            break;
+        }
+        case "":{
+            break;
+        }
+        case "":{
+            break;
+        }
+    }
+}
+
