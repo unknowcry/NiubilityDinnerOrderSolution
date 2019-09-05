4cc34487d51d35b6836c739599d882336e27d6e0
WolfHugo <1031629044@qq.com>
2019-09-05 20:31:52 +0800

    buzhidaoganleshenm

--- test.php
+++ test.php
@@ -2,8 +2,7 @@
 
 require "./sqlinit.php";
 
-
+print_r($listOnTable->getListOnTable('restaurant'));
 $database=new operateDataOnTableFromDatabase($listOnTable->getListOnTable('restaurant'));
-$data=$database->getAllData();
 //$data=$database->getAllData();
 print_r($data);
